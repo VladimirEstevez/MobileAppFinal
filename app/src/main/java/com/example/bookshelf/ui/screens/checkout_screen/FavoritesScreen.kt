584bff257c5d7e8b74d5b6f0fa98947861a2edf6
Vladimir <le0679993@crc-lennox.qc.ca>
2023-12-08 21:54:44 -0500

    wtf wtwfs asdfasd

--- app/src/main/java/com/example/bookshelf/ui/screens/checkout_screen/FavoritesScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/checkout_screen/FavoritesScreen.kt
@@ -14,13 +14,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerFormatter
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,20 +38,28 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bookshelf.R
 import com.example.bookshelf.model.Book
 import com.example.bookshelf.ui.screens.beer_inventory_screen.*
 import com.example.bookshelf.ui.theme.BookshelfTheme
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 import kotlin.math.absoluteValue
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritesScreen(
     viewModel: QueryViewModel,
     modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.uiState.collectAsState(initial = QueryUiState.Loading)
+    var selectedDate by remember { mutableStateOf(LocalDate.now()) }
+    val datePickerState = rememberDatePickerState( initialSelectedDateMillis = Instant.now().toEpochMilli() )
+    var isDatePickerDialogOpen by remember { mutableStateOf(false) }
 
     LaunchedEffect(viewModel) {
         viewModel.getBeers()
@@ -53,9 +70,39 @@ fun FavoritesScreen(
         else -> emptyList() // or handle other states if needed
     }
 
-    LazyColumn(contentPadding = PaddingValues(vertical = 8.dp),) {
-        items(books.size) { book ->
-            FavoritesCard(books[book])
+    Column {
+        
+        if (isDatePickerDialogOpen) {
+            Dialog(onDismissRequest = { isDatePickerDialogOpen = false }) {
+                Column {
+            DatePicker(
+                datePickerState = datePickerState,
+                modifier = Modifier.fillMaxWidth(),
+                dateFormatter = remember { DatePickerFormatter() },
+                dateValidator = { true }, // Your custom date validation logic here
+                title = { Text(text = "Select a pick-up date:") },
+                headline = { }, // Your custom headline composable
+                colors = DatePickerDefaults.colors()
+            )
+            Button(onClick = { isDatePickerDialogOpen = false }) {
+                Text("Confirm Date")
+            }
+        }
+            }
+        }else {
+            Button(onClick = { isDatePickerDialogOpen = !isDatePickerDialogOpen}) {
+            Text("Select a date")
+        }
+        LazyColumn(contentPadding = PaddingValues(vertical = 8.dp)) {
+            items(books.size) { book ->
+                FavoritesCard(books[book])
+            }
+        }
+    }
+
+        LaunchedEffect(datePickerState.selectedDateMillis) {
+            val dateMillis = datePickerState.selectedDateMillis ?: return@LaunchedEffect
+            selectedDate = Instant.ofEpochMilli(dateMillis).atZone(ZoneId.systemDefault()).toLocalDate()
         }
     }
 }
