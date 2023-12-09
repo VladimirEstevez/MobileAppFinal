8c2c1319c15279a74e769c9b35031b31165208ca
TheWhitcher <zwhitcher1995@gmail.com>
2023-12-08 22:22:17 -0500

    wtfwtfwtfwtfwtfwtfwtfwtfwtfwfwtftwftwtfwtf

--- app/src/main/java/com/example/bookshelf/ui/screens/checkout_screen/FavoritesScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/checkout_screen/FavoritesScreen.kt
@@ -2,6 +2,7 @@ package com.example.bookshelf.ui.screens.checkout_screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,15 +13,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,21 +35,23 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bookshelf.R
 import com.example.bookshelf.model.Book
 import com.example.bookshelf.ui.screens.beer_inventory_screen.*
 import com.example.bookshelf.ui.theme.BookshelfTheme
-import kotlin.math.absoluteValue
+import java.time.format.TextStyle
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritesScreen(
     viewModel: QueryViewModel,
     modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.uiState.collectAsState(initial = QueryUiState.Loading)
-
+    val (email, setEmail) = remember { mutableStateOf("") }
     LaunchedEffect(viewModel) {
         viewModel.getBeers()
     }
@@ -53,27 +61,112 @@ fun FavoritesScreen(
         else -> emptyList() // or handle other states if needed
     }
 
-    LazyColumn(contentPadding = PaddingValues(vertical = 8.dp),) {
-        items(books.size) { book ->
-            FavoritesCard(books[book])
+    Box( modifier = Modifier
+        .fillMaxSize()
+    ) {
+        Column() {
+            OrderTotal()
+
+            OrderId()
+
+            EmailInput(email, setEmail)
+
+            Row() {
+                Text(
+                    text = "Order Summary",
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = 10.dp)
+                )
+                LazyColumn(contentPadding = PaddingValues(vertical = 8.dp),) {
+
+                    items(books.size) { book ->
+                        FavoritesCard(books[book])
+                    }
+                }
+            }
+
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailInput(email: String, setEmail: (String) -> Unit) {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Email",
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = 10.dp, start = 10.dp)
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = { newEmail -> setEmail(newEmail) },
+            placeholder = { Text("Enter your email") },
+            //textStyle = androidx.compose.ui.text.TextStyle(fontSize = .sp),
+            modifier = Modifier.width(230.dp).height(60.dp)
+        )
+    }
+}
 
+@Composable
+fun OrderId() {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Text(
+            text = "Order ID",
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = 10.dp, start = 10.dp)
+        )
+
+        // Details label
+        Text(
+            text = "1",
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun OrderTotal() {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+    ){
+        Text(
+            text = "Total Amount",
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = 10.dp, start = 10.dp)
+        )
+
+        // Details label
+        Text(
+            text = "9001",
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
 @Composable
 fun FavoritesCard(item: Book) {
     // Your existing card composable, modified to use data from the item
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .height(120.dp)
+            .height(60.dp)
             .shadow(4.dp),
         shape = MaterialTheme.shapes.medium,
     ) {
         Row(
             modifier = Modifier
-                .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
         ) {
             // Image
@@ -81,7 +174,7 @@ fun FavoritesCard(item: Book) {
                 painter = painterResource(id = R.drawable.fsm),//item.imageRes),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(80.dp)
+                    .size(50.dp)
                     .clip(CircleShape)
                     .padding(8.dp)
                     .align(Alignment.CenterVertically),
@@ -113,7 +206,6 @@ fun FavoritesCard(item: Book) {
         }
     }
 }
-
 
 @Preview(showSystemUi = true)
 @Composable
